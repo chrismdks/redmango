03c6db2e36083bc42b9f6a264c1188b0ea6d25f9
Chrysanthi Michelioudaki <chrysanthi.michelioudaki@ibm.com>
2023-12-18 11:51:32 +0200

    added Routes and NotFound page

--- src/Container/App.tsx
+++ src/Container/App.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Header, Footer } from '../Components/Layout';
-import { Home } from '../Pages';
+import { Home, NotFound } from '../Pages';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
 
   return (
     <div>
       <Header/>
-      <Home/>
+      <div className='pb-5'>
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/shoppingCart" element={<NotFound/>}></Route>
+        </Routes>
+      </div>
       <Footer/>
     </div>
   );
